34ba85588631eea95a4e0353839906c41b558bce
NataliaRovaris <nataliarovaris@hotmail.com>
2025-07-26 20:08:19 -0300

    PUT and DELETE inserted

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const server = http.createServer((request, response) => {
     });
 
     request.on('end', () =>{
+        const id = url.split('/')[2];
+
         if(url === '/grades' && method === 'GET') {
             response.writeHead(200, {'Content-Type': 'application/json'});
             response.end(JSON.stringify(grades));
@@ -23,6 +25,29 @@ const server = http.createServer((request, response) => {
             grades.push(newGrade);
             response.writeHead(201, {'Content-Type': 'application/json'});
             response.end(JSON.stringify(newGrade));
+        } else if(url.startsWith('/grades/') && method === 'PUT') {
+            const { studentName, subject, grade } = JSON.parse(body);
+            const gradesToUpdate = grades.find(g => g.id === id);
+            if (gradesToUpdate) {
+                gradesToUpdate.studentName = studentName;
+                gradesToUpdate.subject = subject;
+                gradesToUpdate.grade = grade;
+                response.writeHead(200, {'Content-Type': 'application/json'});
+                response.end(JSON.stringify(gradesToUpdate));
+            } else {
+                response.writeHead(404, {'Content-Type': 'application/json'});
+                response.end(JSON.stringify({message: "Grade not found"}));
+            }
+        } else if(url.startsWith('/grades/') && method === 'DELETE') {
+            const index = grades.findIndex((g) => g.id === id);
+            if (index !== -1) {
+                grades.splice(index, 1);
+                response.writeHead(204);
+                response.end();
+            } else {
+                response.writeHead(404, {'Content-Type': 'application/json'});
+                response.end(JSON.stringify({message: "Route not found"}));
+            }
         }else {
             response.writeHead(404, {'Content-Type': 'application/json'});
             response.end(JSON.stringify({message: "Route not found"}));
